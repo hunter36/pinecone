97811d5fb5e643af56a4216bbef36061a8445c87
hunter36 <673389081@qq.com>
2018-04-02 13:57:04 +0800

    响应式

--- src/components/search/Search.vue
+++ src/components/search/Search.vue
@@ -1,15 +1,45 @@
 <template>
 	<h1>This is Search module!</h1>
-	<a href="about.html">about</a>
+	<div class="box">
+		<div class="card">
+			<div class="titleBar">卡片</div>
+			<div class="cBody">
+				<div class="cImg"></div>
+			</div>
+		</div>
+		<div class="store"></div>
+		<div class="eshop"></div>
+	</div>
 </template>
 
-<script></script>
+<script>
+	
+</script>
 
 <style scoped>
 	h1{
 		color: rgb(5, 110, 234);
 	}
-	a{
-		font-size:50px;
+	body{
+		background: #F0F2F5;
+	}
+	.box{
+		display: flex;
+		flex-direction: column;
+	}
+	.box div{
+		width: 100%;
+		margin-top: 0.2rem;
+		background: #fff;
+	}
+	.titleBar{
+
+	}
+	.cBody{
+		box-sizing: border-box;
+		padding: 0.39rem 0.3rem 0.4rem;
+	}
+	.cImg{
+		
 	}
 </style>
