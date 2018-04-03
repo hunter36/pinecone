c3d698b98d5f4c0e5e71edf99a103ec6970ac6eb
hunter36 <673389081@qq.com>
2018-04-03 10:37:23 +0800

    卡片区，375以下机型有bug，需要媒体查询

--- src/components/search/Search.vue
+++ src/components/search/Search.vue
@@ -5,8 +5,27 @@
 			<div class="titleBar">卡片</div>
 			<div class="cBody">
 				<div class="cImg">
-					<div class="cLogo">
-						<img src="../../assets/img/卡1图.png" alt="院子餐厅" />
+					<div class="cTop">
+						<dl>
+							<dt><div class="cLogo"></div></dt>
+							<dd>
+								<h3>团圆佳节卡</h3>
+								<p>院子餐厅</p>
+							</dd>
+						</dl>
+						<div class="credit">
+							<p>您的预估额度为</p>
+							<h2><b>￥</b>2300<span>.00</span></h2>
+						</div>
+					</div>
+					<div class="line"></div>
+					<div class="cBottom">
+						<div class="cTips">
+							<p><i><img src="../../assets/img/付.png"></i>支付方式<span>信用支付</span></p>
+							<p><i><img src="../../assets/img/条件.png"></i>申请条件<span>信用支付</span></p>
+							<p><i><img src="../../assets/img/地址.png"></i>适用范围<span>信用支付</span></p>
+							<p><i><img src="../../assets/img/期限.png"></i>有效期限<span>信用支付</span></p>
+						</div>
 					</div>
 				</div>
 			</div>
@@ -24,9 +43,7 @@
 					<li>
 						<div class="sImg"></div>
 					</li>
-					<li>
-						<div class="sImg"></div>
-					</li>
+					<li class="block"></li>
 				</ul>
 			</div>
 		</div>
@@ -45,6 +62,10 @@
 </script>
 
 <style scoped>
+	*{
+		margin: 0;
+		padding: 0;
+	}
 	h1{
 		font-size: 20px;
 		color: rgb(5, 110, 234);
@@ -75,22 +96,103 @@
 		padding: 0 0.3rem 0;
 	}
 	.cImg{
-		position: relative;
 		height: 2.74rem;
+		box-sizing: border-box;
 		background: url(../../assets/img/card3.png) no-repeat center center;
-		background-size: 100%; 
+		background-size: 100%;
+		padding: 0 0.28rem; 
 		margin: .4rem 0;
 		border-bottom: 1px solid #ECECEC;
+		display: flex;
+		flex-direction: column;
+	}
+	.cImg .cTop{
+		box-sizing: border-box;
+		padding: .28rem 0;
+		height: 1.52rem;
+		box-sizing: border-box;
+		overflow: hidden;
+		display: flex;
+		justify-content: space-between;
+	}
+	.cTop dl{
+		font-size: 0;
+		display: flex;
+		justify-content: space-between;
+	}
+	.cTop dl dd{
+		font-size: .28rem;
+		display: flex;
+		flex-direction: column;
+		justify-content: space-around;
+		color: #fefefe;
+		margin-left: .28rem;
+	}
+	.cTop dl dd h3{
+		margin: 0;
+		padding: 0;
+		font-size: .34rem;
+		font-weight: normal;
+	}
+	.cTop dl dd p{
+		margin: 0;
+		padding: 0;
+	}
+	.cTop .credit{
+		text-align: right;
+		font-size: .2rem;
+		color: #fff;
+	}
+	.credit p{
+		color: rgba(255,255,255,.6);
+	}
+	.credit h2{
+		font-size: .48rem;
+		font-weight: normal;
+	}
+	.credit h2 b{
+		font-size: .3rem;
+	}
+	.cBottom{
+		height: 1.19rem;
+		padding: .21rem 0 .3rem;
+		box-sizing: border-box;
+	}
+	.cTips{
+		font-size: .22rem;
+		color: rgba(255,255,255,.6);
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		flex-wrap: wrap;
+	}
+	.cTips p{
+		display: block;
+		width: 50%;
+		overflow: hidden;
+	}
+	.cTips p i{
+		display: inline-block;
+		width: .22rem;
+		height: .22rem;
+		margin-right: .1rem;
+	}
+	.cTips p i img{
+		width: 100%;
+	}
+	.cTips p span{
+		margin-left: .32rem;
+	}
+	.line{
+		width: 100%;
+		height: 1px;
+		background: rgba(255,255,255,0.35);
 	}
 	.cLogo{
 		width: .96rem;
 		height: .96rem;
-		position: absolute;
-		top: .28rem;
-		left: .28rem;
-	}
-	.cLogo img{
-		width: 100%;
+		background: url(../../assets/img/卡1图.png) no-repeat center center;
+		background-size: 100%; 
 	}
 	.eshop{
 		margin-bottom: .6rem;
@@ -103,21 +205,26 @@
 	.sBody ul{
 		margin: 0;
 		padding: 0;
+		width: 100%;
 		overflow: hidden;
 		height: auto;
 		display: flex;
-		justify-content: space-around;
 		flex-wrap: wrap;
+		justify-content: space-around;
 	}
 	.sBody ul li{
 		list-style: none;
+		box-sizing: border-box;
 		width: 3.6rem;
 		height: 4.2rem;
 		background: gold;
 		margin-bottom: .1rem;
+		border: 1px solid #ECECEC;
+		border-radius: 5px;
 	}
-	.sBody ul li:nth-child(2n){
-		margin-right: 0;
+	.sBody ul .block{
+		height: 0;
+		border: none;
 	}
 	.sBody ul li .sImg{
 		width: 3.4rem;
